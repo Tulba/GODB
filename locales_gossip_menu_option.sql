41acbf04bc2dd348858e4d170477f7f602236439
Matthias <matzetulba@aol.com>
2015-03-04 18:14:18 +0100

    ADDED: Converted locales from GMDB -> oregoncore structure.

---
+++ locales_gossip_menu_option.sql
@@ -0,0 +1,87 @@
+SET NAMES 'utf8';
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 0, 'GOSSIP_OPTION_QUESTGIVER', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 1, 'Ich möchte ein wenig in Euren Waren stöbern.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 2, 'Ich brauche einen Flug.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 3, 'Ich benötige eine Ausbildung.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 4, 'GOSSIP_OPTION_SPIRITHEALER', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 5, 'GOSSIP_OPTION_SPIRITGUIDE', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 6, 'Ich möchte dieses Gasthaus zu meinem Heimatort machen.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 7, 'GOSSIP_OPTION_BANKER', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 8, 'Wie gründe ich eine Gilde?', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 9, 'Ich möchte ein Gilddenwappen herstellen.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 10, 'Ich möchte das Schlachtfeld betreten.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 11, 'Zeigt mir eure Auktionen.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 12, 'Ich hätte gern einen Platz im Stall für meinen Begleiter.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 13, 'GOSSIP_OPTION_ARMORER', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 14, 'Ich möchte meine Talente verlernen.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 15, 'Ich möchte, dass mein Begleiter alles verlernt.', NULL);
+
+INSERT INTO locales_gossip_menu_option
+   (`menu_id`, `id`, `option_text_loc3`, `box_text_loc3`)
+VALUES
+   (0, 16, 'Duale Talentspezialisierung erwerben', 'Seid Ihr sicher, dass Ihr eine zweite Talentspezialisierung erwerben möchtet?');
+
